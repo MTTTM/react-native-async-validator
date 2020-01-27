c232f78561669e8c65028b6aa5c849441121ab6e
Boolean <xtaobaoshouji@gmail.com>
2020-01-27 09:42:45 +0800

    添加发布npm步骤

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+/**
+ * npm 发布包的几个流程
+ * 1.npm login
+ * 2.修改package.json的包，选择需要提交的文件（package.json和reade.md是默认的无法忽略）
+ * 3.npm publish --dry-run 测试有效提交文件
+ * 4.npm publish 提交成功
+ */
